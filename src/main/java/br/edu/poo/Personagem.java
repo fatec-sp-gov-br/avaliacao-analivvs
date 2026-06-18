311345fdaaa5b449fa1a5d3eb84106fb77668c82
analivvs <aninha4620@gmail.com>
2026-06-17 21:23:43 -0300

    Refactor Personagem class with new attributes and methods

--- src/main/java/br/edu/poo/Personagem.java
+++ src/main/java/br/edu/poo/Personagem.java
@@ -2,34 +2,54 @@ package br.edu.poo;
 
 public class Personagem {
 
-    public Personagem(String nome,
-                      int vida,
-                      int ataque,
-                      int defesa) {
+    private String nome;
+    private int vida;
+    private int ataque;
+    private int defesa;
+    private boolean pocaoUtilizada;
 
+    public Personagem(String nome, int vida, int ataque, int defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.pocaoUtilizada = false;
     }
 
     public void atacar(Personagem inimigo) {
+        int dano = ataque - inimigo.defesa;
 
+        if (dano < 1) {
+            dano = 1;
+        }
+
+        inimigo.receberDano(dano);
     }
 
     public void receberDano(int dano) {
+        vida -= dano;
 
+        if (vida < 0) {
+            vida = 0;
+        }
     }
 
     public void usarPocao(Pocao pocao) {
-
+        if (!pocaoUtilizada) {
+            vida += pocao.getCura();
+            pocaoUtilizada = true;
+        }
     }
 
     public boolean estaVivo() {
-        return false;
+        return vida > 0;
     }
 
     public String getNome() {
-        return null;
+        return nome;
     }
 
     public int getVida() {
-        return 0;
+        return vida;
     }
 }
