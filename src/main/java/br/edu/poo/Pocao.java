17575507d175cdb9d15135e5b67c9cac273b2170
analivvs <aninha4620@gmail.com>
2026-06-17 21:26:13 -0300

    Update Pocao.java

--- src/main/java/br/edu/poo/Pocao.java
+++ src/main/java/br/edu/poo/Pocao.java
@@ -1,16 +1,20 @@
 package br.edu.poo;
 
 public class Pocao {
+    
+    private String nome;
+    private int cura;
 
     public Pocao(String nome, int cura) {
-
+        this.nome = nome;
+        this.cura = cura;
     }
 
     public String getNome() {
-        return null;
+        return nome;
     }
 
     public int getCura() {
-        return 0;
+        return cura;
     }
 }
