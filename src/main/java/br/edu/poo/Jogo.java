37451d97e681fcf22bc265a2a089882a2c9e9cd4
analivvs <aninha4620@gmail.com>
2026-06-17 21:28:41 -0300

    Add battle simulation between characters
    Implement battle logic between characters with healing.

--- src/main/java/br/edu/poo/Jogo.java
+++ src/main/java/br/edu/poo/Jogo.java
@@ -4,8 +4,38 @@ public class Jogo {
 
     public static void main(String[] args) {
 
-        // Implemente a batalha aqui
+        Personagem artemis = new Personagem("Artemis", 30, 8, 2);
+        Personagem goblin = new Personagem("Goblin", 20, 5, 1);
 
+        Pocao pequena = new Pocao("Poção Pequena", 10);
+
+        System.out.println("=== BATALHA INICIADA ===");
+
+        while (artemis.estaVivo() && goblin.estaVivo()) {
+
+            artemis.atacar(goblin);
+            System.out.println(goblin.getNome() + " possui " + goblin.getVida() + " de vida");
+
+            if (!goblin.estaVivo()) {
+                break;
+            }
+
+            if (goblin.getVida() <= 10) {
+                goblin.usarPocao(pequena);
+                System.out.println(goblin.getNome() + " usa " + pequena.getNome());
+                System.out.println(goblin.getNome() + " possui " + goblin.getVida() + " de vida");
+            }
+
+            goblin.atacar(artemis);
+            System.out.println(artemis.getNome() + " possui " + artemis.getVida() + " de vida");
+        }
+
+        System.out.println("=== FIM DA BATALHA ===");
+
+        if (artemis.estaVivo()) {
+            System.out.println("Vencedor: " + artemis.getNome());
+        } else {
+            System.out.println("Vencedor: " + goblin.getNome());
+        }
     }
-
 }
